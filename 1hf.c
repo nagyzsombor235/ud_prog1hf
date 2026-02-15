99e8942b487a0c7b0b04e26ef6c38520f4ee3407
nagyzsombor235 <zsombornagy560@gmail.com>
2026-02-15 13:22:57 +0100

    Update 1hf.c

--- 1hf.c
+++ 1hf.c
@@ -1,16 +1,19 @@
-#define PI 3.14
 #include <stdio.h>
 #include <math.h>
+
     int main() {
     int a = 0;
     int b=0;
-    printf("Adj meg két számot");
+    printf("Adj meg a téglalap oldalainak a hosszát");
     scanf("%d %d", &a, &b);
     int tegterulet = a*b;
     int tegkerulet = 2*(a+b);
     printf("a négyzet területe %d és a kerülete %d\n", tegterulet, tegkerulet);
-    double korterulet = a*a*PI;
-    double korkerulet = 2*a*PI;
+    printf("Adja meg a kör sugarát");
+    float korsugar = 0; 
+    scanf("%f", &korsugar);
+    double korterulet = pow(korsugar, 2)*M_PI;
+    double korkerulet = 2*korsugar*M_PI;
     printf("A kör területe %f a kerülete pedig %f\n", korterulet, korkerulet);
     return 0;
     }
