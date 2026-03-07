1c55efd4a6a68ebfe9d9e652d1a9cf4031e22341
nagyzsombor235 <zsombornagy560@gmail.com>
2026-03-07 14:44:55 +0100

    Create 305e.c

---
+++ 305e.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int is_even(int n)
+{
+    if (n%2==0)
+    {
+        return 1;
+    }
+    else
+    return 0;
+    
+}
+
+
+int is_odd(int n)
+{
+    if (n%2==1)
+    {
+        return 1;
+    }
+    else
+    return 0;
+    
+}
+
+int is_odd2(int n)
+{
+    if (is_even(n)==0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+    
+
+int main()
+{
+    printf("%d\n",is_even(1));
+    printf("%d\n",is_odd(1));
+    printf("%d\n",is_odd2(2));
+}
