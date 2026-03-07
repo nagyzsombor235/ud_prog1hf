a05cffbff98b65421100264bdac3573ab3caa192
nagyzsombor235 <zsombornagy560@gmail.com>
2026-03-07 14:44:58 +0100

    Create index.c

---
+++ index.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int ind(int t[],int n,int p)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (t[i] == p)
+        {
+            return i;
+        }
+    }
+    return 0;
+    
+}
+
+int main()
+{
+    int tomb[7]={0,1,2,3,4,5,6};
+    printf("%d",ind(tomb,7,3));
+}
