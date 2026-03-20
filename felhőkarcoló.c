376613722023f5f564f3658048852e4b153a3880
nagyzsombor235 <zsombornagy560@gmail.com>
2026-03-20 22:21:56 +0100

    Update felhőkarcoló.c

--- felhőkarcoló.c
+++ felhőkarcoló.c
@@ -20,6 +20,6 @@ int skyscraper(int t[],int hossz)
 
 int main()
 {
-    int tomb[8]={0,1,2,3,4,5,18,7};
-    printf("%d",skyscraper(tomb,8));
+    int tomb[38]={0,1,2,3,4,5,18,7,5,1,2,3,5,7,5,4,3,5,6,7,8,4,32,3,4,5,6,7,8,9,7,42,6,34,5,6,7,8};
+    printf("%d",skyscraper(tomb,38));
 }
